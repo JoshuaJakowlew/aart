a77d244ed06f442bb9bf78b94dd0578e135dac21
JoshuaJakowlew <jakowlew@pm.me>
2020-01-24 19:54:33 +0300

    Add custom types for color handling Provide basic functions for color conversions and approximations

--- aart/main.cpp
+++ aart/main.cpp
@@ -1,40 +1,163 @@
-#include <iostream>
 #include <numeric>
 #include <opencv2/opencv.hpp>
 
+// Declare generic type for rgb color space
+
+template <typename T>
+struct rgb_t
+{
+	using value_type = T;
+
+	constexpr rgb_t(T r, T g, T b) noexcept :
+		r{ r },
+		g{ g },
+		b{ b }
+	{}
+
+	T r{};
+	T g{};
+	T b{};
+};
+
+namespace cv {
+	template<typename T>
+	struct DataType<rgb_t<T>>
+	{
+		using value_type   = rgb_t<T>;
+		using work_type    = typename DataType<T>::work_type;
+		using channel_type = T;
+
+		enum {
+			generic_type = 0,
+			channels     = 3,
+			fmt          = traits::SafeFmt<channel_type>::fmt + ((channels - 1) << 8)
+		};
+
+		using vec_type = Vec<channel_type, channels>;
+	};
+
+	namespace traits {
+		template<typename T>
+		struct Depth<rgb_t<T>>
+		{
+			enum { value = Depth<T>::value };
+		};
+
+		template<typename T>
+		struct Type<rgb_t<T>>
+		{
+			enum { value = CV_MAKETYPE(Depth<T>::value, 3) };
+		};
+	}
+}
+
+// Declare generic type for lab color space
+
+template <typename T>
+struct lab_t
+{
+	using value_type = T;
+
+	constexpr lab_t(T l, T a, T b) noexcept :
+		l{ l },
+		a{ a },
+		b{ b }
+	{}
+
+	T l{};
+	T a{};
+	T b{};
+};
+
+namespace cv {
+	template<typename T>
+	struct DataType<lab_t<T>>
+	{
+		using value_type = lab_t<T>;
+		using work_type = typename DataType<T>::work_type;
+		using channel_type = T;
+
+		enum {
+			generic_type = 0,
+			channels = 3,
+			fmt = traits::SafeFmt<channel_type>::fmt + ((channels - 1) << 8)
+		};
+
+		using vec_type = Vec<channel_type, channels>;
+	};
+
+	namespace traits {
+		template<typename T>
+		struct Depth<lab_t<T>>
+		{
+			enum { value = Depth<T>::value };
+		};
+
+		template<typename T>
+		struct Type<lab_t<T>>
+		{
+			enum { value = CV_MAKETYPE(Depth<T>::value, 3) };
+		};
+	}
+}
+
+[[nodiscard]] auto normalize(cv::Mat& img) noexcept -> void
+{
+	img /= 255.0;
+}
+
+[[nodiscard]] auto bgr2lab(const cv::Mat& img) noexcept -> cv::Mat
+{
+	cv::Mat result;
+	img.convertTo(result, CV_32F);
+	normalize(result);
+	cv::cvtColor(std::move(result), result, cv::COLOR_BGR2Lab);
+	return result;
+}
+
+template <typename T>
+[[nodiscard]] constexpr auto CIE76_distance(const lab_t<T>& x, const lab_t<T>& y) noexcept -> T
+{
+	return sqrt(pow(x.l - y.l, 2) + pow(x.a - y.a, 2) + pow(x.b - y.b, 2));
+}
+
+template <typename T, typename F>
+[[nodiscard]] auto similar(const T& goal, const cv::Mat& palette, F distance) noexcept -> T
+{
+	return std::reduce(palette.begin<T>() + 1, palette.end<T>(), *palette.begin<T>(),
+		[&goal, distance] (auto&& prev, auto&& curr) noexcept
+		{
+			const auto prevD = distance(goal, std::forward<decltype(prev)>(prev));
+			const auto currD = distance(goal, std::forward<decltype(curr)>(curr));
+			return currD < prevD ? curr : prev;
+		}
+	);
+
+	/*auto dmin = distance(goal, *palette.begin<T>());
+	auto result = *palette.begin<T>();
+
+	std::for_each(palette.begin<T>() + 1, palette.end<T>(), [&](auto color) noexcept {
+		const auto dcolor = distance(goal, color);
+		if (dcolor < dmin)
+		{
+			dmin = dcolor;
+			result = color;
+		}
+	});
+
+	return result;*/
+}
+
 int main()
 {
-	using Pixel = cv::Point3_<float>;
+	auto palette = bgr2lab(cv::imread("palette.bmp", cv::IMREAD_COLOR));
 
-    auto palette1 = cv::imread("palette.bmp", cv::IMREAD_COLOR);
-	cv::Mat palette;
-	palette1.convertTo(palette, CV_32F);
-	palette *= 1.0 / 255.0;
-    cv::cvtColor(palette, palette, cv::COLOR_BGR2Lab);
-
-	auto pic1 = cv::imread("test.jpg", cv::IMREAD_COLOR);
-	cv::Mat pic;
-	pic1.convertTo(pic, CV_32F);
-	pic *= 1.0 / 255.0;
+	auto pic = cv::imread("test.jpg", cv::IMREAD_COLOR);
 	auto orig = pic.clone();
-	auto test = *pic.begin<Pixel>();
-	cv::cvtColor(pic, pic, cv::COLOR_BGR2Lab);
-	auto test1 = *pic.begin<Pixel>();
 
-	auto dist = [](Pixel y, Pixel x) {
-		return sqrt(pow(x.x - y.x, 2) + pow(x.y - y.y, 2) + pow(x.z - y.z, 2));
-	};
-
-	auto similar = [&palette, dist](const Pixel& goal) {
-		return std::accumulate(palette.begin<Pixel>() + 1, palette.end<Pixel>(), *palette.begin<Pixel>(), [&goal, dist](auto prev, auto curr) {
-			auto currD = dist(goal, curr);
-			auto prevD = dist(goal, prev);
-			return (currD < prevD ? curr : prev);
-		});
-	};
-
-	pic.forEach<Pixel>([&palette, similar](Pixel& p, const int* pos) {
-		p = similar(p);
+	pic = bgr2lab(pic);
+	pic.forEach<lab_t<float>>([&palette](lab_t<float>& p, const int* pos) {
+		p = similar(p, palette, CIE76_distance<float>);
 	});
 
 	cv::cvtColor(pic, pic, cv::COLOR_Lab2BGR);
