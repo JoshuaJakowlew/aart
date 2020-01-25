a306e7320bd6ab904325e7c6f0af6a057d257fdf
JoshuaJakowlew <jakowlew@pm.me>
2020-01-25 04:34:37 +0300

    Very first and dirty full conversion implementation

--- aart/main.cpp
+++ aart/main.cpp
@@ -148,21 +148,73 @@ template <typename T, typename F>
 	return result;*/
 }
 
+template <typename T, typename F>
+[[nodiscard]] auto similar2(const T& goal, const cv::Mat& palette, F distance) noexcept -> std::tuple<T, T, int, int>
+{
+	auto dmin1 = distance(goal, *palette.begin<T>());
+	auto dmin2 = dmin1;
+	auto c1 = palette.begin<T>();
+	auto c2 = c1;
+
+	
+	for (auto c = palette.begin<T>() + 1; c != palette.end<T>(); ++c)
+	{
+		auto delta = distance(goal, *c);
+
+		if (delta < dmin1) {
+			dmin2 = dmin1;
+			dmin1 = delta;
+
+			c2 = c1;
+			c1 = c;
+		}
+		else if (delta < dmin2) {
+			dmin2 = delta;
+
+			c2 = c;
+		}
+	}
+
+	return std::make_tuple(*c1, *c2, c1 - palette.begin<T>(), c2 - palette.begin<T>());
+}
+
 int main()
 {
-	auto palette = bgr2lab(cv::imread("palette.bmp", cv::IMREAD_COLOR));
+	auto colormap = bgr2lab(cv::imread("colormap.png", cv::IMREAD_COLOR));
+	auto charmap = cv::imread("charmap.png", cv::IMREAD_COLOR);
+
+	constexpr auto n_chars = 10; // 10 in current palette " .:-=+*#%@"
+	const auto n_colors = colormap.size().width;
+	const auto char_w = charmap.size().width / n_chars;
+	const auto char_h = charmap.size().height / (n_colors * n_colors);
+	const auto n_charmap = n_chars * n_colors * n_colors;
 
 	auto pic = cv::imread("test.jpg", cv::IMREAD_COLOR);
-	auto orig = pic.clone();
+	cv::resize(pic, pic, {}, 1.0, (double)char_w / char_h, cv::INTER_LINEAR);
+	auto art = cv::Mat(pic.size().height * char_h, pic.size().width * char_w, charmap.type());
 
 	pic = bgr2lab(pic);
-	pic.forEach<lab_t<float>>([&palette](lab_t<float>& p, const int* pos) {
-		p = similar(p, palette, CIE76_distance<float>);
+	pic.forEach<lab_t<float>>([&colormap, &charmap, &art, char_w, char_h, n_chars, n_colors](lab_t<float>& p, const int* pos) {
+		auto [c1, c2, i1, i2] = similar2(p, colormap, CIE76_distance<float>);
+		auto d1 = CIE76_distance(p, c1);
+		auto d2 = CIE76_distance(p, c2);
+		int ic = d2 == 0 ? n_chars - 1 : d1 / d2 * (n_chars - 1);
+
+		auto y = pos[0];
+		auto x = pos[1];
+
+		auto sx = ic * char_w;
+		auto sy = i1 * char_h * n_colors + i2 * char_h;
+
+		auto symbol = charmap.colRange(sx, sx + char_w).rowRange(sy, sy + char_h);
+		auto dst = art.colRange(x * char_w, (x + 1) * char_w).rowRange(y * char_h, (y + 1) * char_h);
+		symbol.copyTo(dst);
 	});
 
 	cv::cvtColor(pic, pic, cv::COLOR_Lab2BGR);
 
-	cv::imshow("1", orig);
-	cv::imshow("2", pic);
-	cv::waitKey(0);
+	//cv::imshow("1", pic);
+	//cv::imshow("2", art);
+	cv::imwrite("result.png", art);
+	//cv::waitKey(0);
 }
