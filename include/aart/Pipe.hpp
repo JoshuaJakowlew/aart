37fcae7e4816e705d6d67dad01dbe5369c104804
Jakowlew <jakowlew@pm.me>
2021-03-05 02:03:31 +0300

    Implement pipes

---
+++ include/aart/Pipe.hpp
@@ -0,0 +1,64 @@
+#ifndef AART_PIPE_H
+#define AART_PIPE_H
+
+#include <aart/IFilter.hpp>
+
+namespace detail {
+    template<typename, typename>
+    struct PipeNode {};
+
+    template<Filter T, Filter U>
+    struct PipeNode<T, U> {
+        using input_t = typename T::resource_t;
+        using output_t = decltype(std::declval<U>()(std::declval<T>()(std::declval<input_t&>())));
+
+        T left;
+        U right;
+
+        decltype(auto) operator()(input_t& frame) const
+        {
+            return right(left(frame));
+        }
+    };
+
+    template<typename Pipe, Filter T>
+        requires std::is_convertible_v<typename Pipe::output_t, typename T::resource_t>
+    struct PipeNode<Pipe, T> {
+        using input_t = typename Pipe::input_t;
+        using output_t = decltype(std::declval<T>()(std::declval<Pipe>()(std::declval<input_t&>())));
+
+        Pipe left;
+        T right;
+
+        decltype(auto) operator()(input_t& frame) const
+        {
+            return right(left(frame));
+        };
+    };
+} // namespace detail
+
+template<Filter T, Filter U>
+auto addToPipe(T&& left, U&& right)
+{
+    return detail::PipeNode<T, U>{std::move(left), std::move(right)};
+}
+
+template<typename Pipe, Filter U>
+auto addToPipe(Pipe&& left, U&& right)
+{
+    return detail::PipeNode<Pipe, U>{std::move(left), std::move(right)};
+}
+
+template<Filter T, Filter U>
+auto operator | (T&& left, U&& right)
+{
+    return addToPipe(std::move(left), std::move(right));
+}
+
+template<typename Pipe, Filter U>
+auto operator | (Pipe&& left, U&& right)
+{
+    return addToPipe(std::move(left), std::move(right));
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <tuple>
 
 #include <aart/utils.h>
+#include <aart/Pipe.hpp>
 #include <aart/ImageManager.hpp>
 #include <aart/ScaleFilter.hpp>
 #include <aart/GrayscaleFilter.hpp>
@@ -20,65 +21,10 @@ void show(const cv::Mat& x)
     cv::destroyWindow("Hello!");
 }
 
-template<typename, typename>
-struct PipeNode {};
-
-template<Filter T, Filter U>
-struct PipeNode<T, U> {
-    using input_t = typename T::resource_t;
-    using return_t = decltype(std::declval<U>()(std::declval<T>()(std::declval<input_t&>())));
-
-    T left;
-    U right;
-
-    decltype(auto) operator()(input_t& frame) const
-    {
-        return right(left(frame));
-    }
-};
-
-template<typename Pipe, Filter T>
-    requires std::is_convertible_v<typename Pipe::return_t, typename T::resource_t>
-struct PipeNode<Pipe, T> {
-    using input_t = typename Pipe::input_t;
-    using return_t = decltype(std::declval<T>()(std::declval<Pipe>()(std::declval<input_t&>())));
-
-    Pipe left;
-    T right;
-
-    decltype(auto) operator()(input_t& frame) const
-    {
-        return right(left(frame));
-    };
-};
-
-template<Filter T, Filter U>
-auto addToPipe(T&& left, U&& right) {
-    return PipeNode<T, U>{std::move(left), std::move(right)};
-}
-
-template<typename Pipe, Filter U>
-auto addToPipe(Pipe&& left, U&& right) {
-    return PipeNode<Pipe, U>{std::move(left), std::move(right)};
-}
-
 int main()
 {
     ImageManager imageManager;
     auto img = imageManager.read("C:/Users/jakow/Documents/Programming/aart/test.png");
-    show(img);
-
-    //ScaleFilter scaleFilter{0.5f, 0.5f};
-    //scaleFilter(img);
-
-    //GrayscaleFilter grayscaleFilter{};
-    //grayscaleFilter(img);
-
-    //auto pipe = addToPipe(ScaleFilter{0.5f, 0.5f}, GrayscaleFilter{});
-    //auto pipe2 = addToPipe(std::move(pipe), ScaleFilter{2.f, 2.f});
-    //auto pipe3 = addToPipe(std::move(pipe2), ScaleFilter{2.f, 2.f});
-    auto pipe = addToPipe(addToPipe(addToPipe(ScaleFilter{0.5f, 0.5f}, GrayscaleFilter{}), ScaleFilter{2.f, 2.f}), ScaleFilter{2.f, 2.f});
-    pipe(img);
-    show(img);
-
+    auto pipe = ScaleFilter{0.5f, 0.5f} | GrayscaleFilter{} | ScaleFilter{2.f, 2.f} | ScaleFilter{2.f, 2.f};
+    show(pipe(img));
 }
